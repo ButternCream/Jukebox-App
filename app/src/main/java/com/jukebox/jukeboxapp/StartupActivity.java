43454b8a84aab1cb68246c34f8bf18491bb0d619
Nick <n.armour0@gmail.com>
2016-03-05 13:32:46 -0800

    buttons change layouts

--- app/src/main/java/com/jukebox/jukeboxapp/StartupActivity.java
+++ app/src/main/java/com/jukebox/jukeboxapp/StartupActivity.java
@@ -2,12 +2,34 @@ package com.jukebox.jukeboxapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartupActivity extends AppCompatActivity {
+    private Button hostButton;
+    private Button shareButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        hostButton = (Button)findViewById(R.id.host_button);
+        shareButton = (Button)findViewById(R.id.share_button);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.host_startup);
+            }
+        });
+
+        shareButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setContentView(R.layout.share_startup);
+            }
+        });
+
+
     }
 }
